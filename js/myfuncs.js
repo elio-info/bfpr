ca65de4cd1c7950ffe58c897820b08f0cf383cfe
elio-info <elio@upr.edu.cu>
2023-06-23 18:59:51 -0400

    se ha avanzado

--- js/myfuncs.js
+++ js/myfuncs.js
@@ -1,5 +1,28 @@
 // JavaScript Document
-function openResult(evt, cityName) {
+function openResult(evt, selectedName) {
+  var i, tabcontent, tablinks;
+  let patron=selectedName.slice(0,2);//tomar los 2 primeros
+  tabcontent = document.getElementsByClassName("tabSpecialcontent");
+  for (i = 0; i < tabcontent.length; i++) {//mov over tabsSpecialcontent
+    let selectedTabSpecialContent =tabcontent[i].id.indexOf(patron);
+    if (selectedTabSpecialContent==0) { //si  cumple con lo que busco
+        tabcontent[i].style.display = 
+        tabcontent[i].id==
+        selectedName? "block":"none";//se lleva oculto si no es         
+    }     
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {//mov over tabLinks
+    let selectedTabLinks=tablinks[i].attributes["onclick"].value.indexOf("'"+patron);//pos on string
+    if (selectedTabLinks>-1) { //exist
+       tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }   
+  }
+  //el actual esta activo
+  evt.currentTarget.className += " active";
+}
+// my original
+function open_Result(evt, cityName) {
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabSpecialcontent");
   for (i = 0; i < tabcontent.length; i++) {
@@ -12,7 +35,6 @@ function openResult(evt, cityName) {
   document.getElementById(cityName).style.display = "block";
   evt.currentTarget.className += " active";
 }
-
 //pensar que hacer para mostrar un lightbox
 function get_elements_for(nombre,clase) {//tomar los elementos de 
   switch (clase) {
